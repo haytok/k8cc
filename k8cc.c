4e5af654eadb86c575d28a481f93eb8b44ea7361
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-02-23 23:05:12 +0900

    update compiler

--- k8cc.c
+++ k8cc.c
@@ -10,7 +10,20 @@ int main(int argc, char *argv[]) {
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+
+    char *p = argv[1];
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    while (*p) {
+        if (*p == '+') {
+            *p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+        }
+        if (*p == '-') {
+            *p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+        }
+    }
+
     printf("  ret\n");
     printf("\n");
     return 0;
--- tmp.s
+++ tmp.s
@@ -1,6 +1,8 @@
 .intel_syntax noprefix
 .global main
 main:
-  mov rax, 42
+  mov rax, 5
+  add rax, 20
+  sub rax, 4
   ret
 
