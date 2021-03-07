907236eca2ff8ba66aa932a0d20f234098d3c3aa
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-03-07 18:25:21 +0900

    update test case

--- test.sh
+++ test.sh
@@ -4,6 +4,11 @@
 cat <<EOF | gcc -xc -c -o tmp2.o -
 int ret3() { return 3; }
 int ret5() { return 5; }
+int add(int x, int y) { return x+y; }
+int sub(int x, int y) { return x-y; }
+int add6(int a, int b, int c, int d, int e, int f) {
+  return a+b+c+d+e+f;
+}
 EOF
 
 assert() {
@@ -102,5 +107,10 @@ assert 55 "i=0; j=0; while(i<=10) {j=i+j; i=i+1;} return j;"
 # 引数が無い関数のテストケース
 assert 3 "return ret3();"
 assert 5 "return ret5();"
+
+# 引数が 6 個までの関数のテストケース
+assert 8 "return add(3, 5);"
+assert 2 "return sub(5, 3);"
+assert 21 "return add6(1,2,3,4,5,6);"
 
 echo OK
