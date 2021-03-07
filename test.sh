8c13c0c67329aa7b5a0a2259d7cbe405537b74bc
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-03-07 13:24:57 +0900

    update test case to compile block

--- test.sh
+++ test.sh
@@ -89,4 +89,8 @@ assert 55 "i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j;"
 assert 3 "for (;;) return 3; return 5;"
 assert 40 "i=0; j=0; for (i=0; i<=10; i=i+1) if (i>5) j = i+j; return j;"
 
+# ブロックのテストケース
+assert 3 "{1; {2;} return 3;}"
+assert 55 "i=0; j=0; while(i<=10) {j=i+j; i=i+1;} return j;"
+
 echo OK
