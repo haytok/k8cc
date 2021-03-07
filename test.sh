97aad40a0b3965d1d04d38a0b50738f607da7ae0
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-03-07 13:15:24 +0900

    update test case

--- test.sh
+++ test.sh
@@ -87,5 +87,6 @@ assert 10 "i=0; while(i<10) i=i+1; return i;"
 # for statement のテストケース
 assert 55 "i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j;"
 assert 3 "for (;;) return 3; return 5;"
+assert 40 "i=0; j=0; for (i=0; i<=10; i=i+1) if (i>5) j = i+j; return j;"
 
 echo OK
